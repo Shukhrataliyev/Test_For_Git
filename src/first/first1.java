31f7a9b5142b6fd67c0e7e3ba7943a20a9f3cd05
Aliyev <saliyev@gmail.com>
2018-07-19 16:52:47 -0500

    added print statement

--- src/first/first1.java
+++ src/first/first1.java
@@ -5,6 +5,9 @@ public class first1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first");
+		
+		System.out.println("alsdlalsdalksdlaksdasd");
+		
 	}
 
 }
